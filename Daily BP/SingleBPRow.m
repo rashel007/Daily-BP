e6e4586fb8ff94c913f7c2480a1952af3d1ca564
EA Rashel <rashel320@gmail.com>
2016-10-16 16:14:22 +0600

    first commit

---
+++ Daily BP/SingleBPRow.m
@@ -0,0 +1,17 @@
+//
+//  SingleBPRow.m
+//  Daily BP
+//
+//  Created by Estique on 10/16/16.
+//  Copyright © 2016 Estique. All rights reserved.
+//
+
+#import "SingleBPRow.h"
+
+@interface SingleBPRow ()
+
+@end
+
+@implementation SingleBPRow
+
+@end
